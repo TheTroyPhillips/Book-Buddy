462db5bbc20d20b771655c8ae01034adc5d74d76
tphilly <troy.phillips44@gmail.com>
2024-01-24 17:12:28 -0500

    login commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const Login = ({ setIsLoggedIn }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    const result = response.json();
+    console.log(result);
+    sessionStorage.setToken("token", result.token);
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <>
+      <div className="formBox">
+        <h2 id="login">Login</h2>
+        <div>
+          <label id="direction">Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label id="direction">Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Login;
